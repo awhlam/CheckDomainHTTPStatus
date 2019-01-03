9dadc32defdc5f7642eec8fb3c77e63470fb30e1
Andrew Lam <32132177+awhlam@users.noreply.github.com>
2019-01-02 20:09:56 -0800

    Update and rename CheckDomainStatus.py to check-domain-http-status.py

---
+++ check-domain-http-status.py
@@ -0,0 +1,40 @@
+# Check HTTP status code of domains names in Excel file
+
+import requests
+import pandas as pd
+
+if __name__ == "__main__":
+    # Import domains column of Excel file to list
+    inputDomainFile = 'DomainList.xlsx'
+    df = pd.read_excel(inputDomainFile, sheet_name=0)
+    domains_list = df['Domain_Name'].tolist()
+
+    # Convert domains in list to strings
+    domains_list_string = [str(domain) for domain in domains_list]
+    # Add http:// in front of domain if the domain doesn't have it already
+    domains_list_string_http = []
+
+    for domain in domains_list_string:
+        domains_http = domain
+        if domain[:7] != 'http://':
+            domains_http = 'http://' + domain
+        domains_list_string_http.append(domains_http)
+
+    # Loop through domains list and check HTTP status code
+    # https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+    domains_statuses = {}
+
+    for idx, domain in enumerate(domains_list_string_http):
+        print(idx)
+        try:
+            request = requests.head(domain)
+        except:
+            domains_statuses[domain] = 'Error'
+        else:
+            domains_statuses[domain] = request.status_code
+
+    # Save dictionary to Pandas dataframe
+    df_domains = pd.DataFrame(list(domains_statuses.items()), columns=['Domain', 'Status'])
+
+    # Write Pandas dataframe to CSV
+    df_domains.to_csv('domain_statuses.csv', sep=',')
